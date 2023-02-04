8eea639f64f154d46f1bce35a89ab2b5ccf09e42
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-02-04 00:17:37 -0500

    feat(internal): `parseJSON`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/internal/parse-json.ts
@@ -0,0 +1,28 @@
+/**
+ * @file Internal - parseJSON
+ * @module tsconfig-utils/internal/parseJSON
+ */
+
+import type { JsonValue } from '@flex-development/tutils'
+import stripBom from 'strip-bom'
+import stripComments from 'strip-json-comments'
+import validateString from './validate-string'
+
+/**
+ * Parses a JSON string.
+ *
+ * Strips comments and [UTF-8 byte order marks (BOM)][1] before parsing.
+ *
+ * [1]: https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8
+ *
+ * @template T - Parsed JSON value type
+ *
+ * @param {string} json - JSON string to parse
+ * @return {T} Parsed JSON value
+ */
+function parseJSON<T extends JsonValue>(json: string): T {
+  validateString(json, 'json')
+  return JSON.parse(stripComments(stripBom(json), { whitespace: false })) as T
+}
+
+export default parseJSON
